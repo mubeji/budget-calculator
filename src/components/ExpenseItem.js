beb648fc6b03a6cce205e05c5043812f616eddcc
wisdom chikwanda <wisdomchikwanda@wisdoms-air.lan>
2019-08-01 05:10:38 +0100

    buttons done

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,25 @@
 
 import React from 'react'
+import {MdEdit, MdDelete} from 'react-icons/md'
 
-const ExpenseItem = () => {
+const ExpenseItem = ({ expense }) => {
+
+    const {id, charge, amount} = expense
     return (
-        <div>
-            Expense Item
-        </div>
+        <li className="item">
+            <div className="info">
+                <span className="expenses">{charge}</span>
+                <span className="amount">${amount}</span>
+            </div>
+            <div>
+                <button className="edit-btn" aria-label="edit button">
+                    <MdEdit />
+                </button>
+                <button className="clear-btn" aria-label="delete button">
+                    <MdDelete />
+                </button>
+            </div>
+        </li>
     )
 }
 
