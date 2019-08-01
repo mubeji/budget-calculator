a9ace0a55581fdc82ee844ed5cc925c2534c9411
wisdom chikwanda <wisdomchikwanda@wisdoms-air.lan>
2019-08-01 03:49:31 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import uuid from 'uuid/v4'
+
+import "./App.css"
+import ExpenseList from './components/ExpenseList'
+import ExpenseForm from './components/ExpenseForm'
+import Alert from './components/Alert'
+
+// Dummy data
+const initialExpense = [
+    {id:uuid(), expense:"rent", amount: 1600},
+    {id:uuid(), expense:"car payment", amount: 400},
+    {id:uuid(), expense:"credit card bill", amount: 1200}
+]
+console.log(initialExpense)
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Alert />
+            <ExpenseForm />
+            <ExpenseList />
+        </>
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const ExpenseItem = () => {
+    return (
+        <div>
+            Expense Item
+        </div>
+    )
+}
+
+export default ExpenseItem
