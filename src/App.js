a49d20c2e2ba03e8bcb0a8d9a9a76c3f1a061993
wisdom chikwanda <wisdomchikwanda@wisdoms-air.lan>
2019-08-01 04:53:41 +0100

    stage one

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import uuid from 'uuid/v4'
 
 import "./App.css"
@@ -7,19 +7,41 @@ import ExpenseForm from './components/ExpenseForm'
 import Alert from './components/Alert'
 
 // Dummy data
-const initialExpense = [
+const initialExpenses = [
     {id:uuid(), expense:"rent", amount: 1600},
     {id:uuid(), expense:"car payment", amount: 400},
     {id:uuid(), expense:"credit card bill", amount: 1200}
 ]
-console.log(initialExpense)
+//console.log(initialExpense)
 
 function App() {
+    // console.log(useState())
+    // To access the state
+    // const result = useState(initialExpenses)
+    // const expenses = result[0]
+    // const setExpenses = result[1]
+    // console.log(expenses)
+    // console.log(setExpenses)
+
+    // Create 2 variable state value and set (function that controls the state), 
+    // using array destracturing, position 0 and 1 in the array
+    const [expenses, setExpenses] = useState(initialExpenses)
+
     return (
         <>
             <Alert />
-            <ExpenseForm />
-            <ExpenseList />
+            <h1>budget calculator</h1>
+            <main className="App">
+                <ExpenseForm />
+                <ExpenseList expenses={expenses}/>
+            </main>
+            <h1>
+                total expenses: <span className="total">
+                    $ {expenses.reduce((acc, crr)=> {
+                        return (acc + crr.amount)
+                    },0)}
+                </span>
+            </h1>
         </>
     )
 }
